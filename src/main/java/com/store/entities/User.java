6773a16761c2e28d034de560757264099634157d
Roger Silva <rogersilvati@gmail.com>
2021-12-03 02:07:45 -0300

    H2 database, test profile, JPA

--- src/main/java/com/store/entities/User.java
+++ src/main/java/com/store/entities/User.java
@@ -8,17 +8,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class User implements Serializable{
-	
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	
+
+	@Id
 	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
