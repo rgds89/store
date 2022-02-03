a48c0c63d204147362babd8b2bd00e23833f724c
Roger Silva <rogersilvati@gmail.com>
2022-02-03 16:32:49 -0300

    User insert

--- src/main/java/com/store/resources/UserResource.java
+++ src/main/java/com/store/resources/UserResource.java
@@ -4,28 +4,35 @@ import com.store.entities.User;
 import com.store.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		List<User> users =  userService.findAll();
-		return ResponseEntity.ok().body(users);
-	}
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
-		return ResponseEntity.ok().body(userService.findById(id));
-	}
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(userService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
 
 }
--- src/main/java/com/store/services/UserService.java
+++ src/main/java/com/store/services/UserService.java
@@ -4,6 +4,7 @@ import com.store.entities.User;
 import com.store.repositories.UserRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,5 +20,9 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> user = userRespository.findById(id);
         return user.get();
+    }
+
+    public User insert (User obj){
+        return userRespository.saveAndFlush(obj);
     }
 }
